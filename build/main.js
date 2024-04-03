5f518fb4f15c16daed0711d43e773f548da7e476
sofianemazieres <100087250+S0fian3M@users.noreply.github.com>
2024-04-03 09:39:53 +0200

    Add responsive web design

--- build/main.js
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { App } from './App.js';
-ReactDOM.createRoot(document.getElementById('root')).render( /*#__PURE__*/React.createElement(App, null));
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+/**
+ * This code is based on dbousamra's Connections clone: https://github.com/dbousamra/connections.
+ */
 import * as React from 'react';
 import {
   Alert,
@@ -173,118 +176,100 @@ export const App = () => {
     const handleClose = () => setIsOpen(false);
 
     return (
-    <ChakraProvider>
-        <Flex h="100vh" w="100vw" align="center" justify="center">
+        <ChakraProvider>
+            <Flex direction="column" align="center" justify="center" minHeight="100vh">
                 <Stack spacing={4} align="center">
-            <Heading size="3xl" fontFamily="Georgia" fontWeight="light">
-            The French Connections
-            </Heading>
+                    <Heading size="3xl" fontFamily="Georgia" fontWeight="light">
+                        The French Connections
+                    </Heading>
                     <Text fontWeight="semibold">Cr&eacute;e 4 groupes de 4 mots !</Text>
-            {game.oneAway && <Alert status='info' variant='left-accent'>
-                <AlertIcon />Presque...</Alert>
-                    }
-            {game.alreadyGuessed && <Alert status='info' variant='left-accent'>
-                <AlertIcon />D&eacute;j&agrave; devin&eacute;...</Alert>
-            }
+                    {game.oneAway && <Alert status='info' variant='left-accent' w={['368px', '448px', '528px', '624px']}>
+                        <AlertIcon />Presque...
+                    </Alert>}
+                    {game.alreadyGuessed && <Alert status='info' variant='left-accent' w={['368px', '448px', '528px', '624px']}>
+                        <AlertIcon />D&eacute;j&agrave; devin&eacute;...
+                    </Alert>}
                     <Modal isOpen={isOpen} onClose={handleClose}>
                         <ModalOverlay />
                         <ModalContent>
-                            <ModalHeader>R&egrave;gles du jeu</ModalHeader>
+                            <ModalHeader fontWeight='bold' fontSize="2xl">R&egrave;gles du jeu</ModalHeader>
                             <ModalCloseButton />
                             <ModalBody>
-                                <Text fontWeight='bold' mb='1rem'>Trouve des groupes de 4 mots qui partagent quelque chose en commun !</Text>
+                                <Text fontWeight='bold'>Trouve des groupes de 4 mots qui partagent quelque chose en commun ! Un nouveau puzzle tous les lundis et vendredis.</Text>
                                 <UnorderedList>
                                     <ListItem>S&eacute;lectionne 4 mots puis appuie sur le bouton "Valider" pour v&eacute;rifier si tu as raison.</ListItem>
                                     <ListItem>Trouve les groupes en faisant moins de 4 erreurs.</ListItem>
                                 </UnorderedList>
+                                <Text fontWeight='bold' mt='1rem'>Exemples de cat&eacute;gories :</Text>
+                                <UnorderedList>
+                                    <ListItem>SERPENTS : Boa, Vip&egrave;re, Crotale, Python </ListItem>
+                                    <ListItem>FEU + _ : Rouge, Follet, Gr&eacute;geois, Sacr&eacute; </ListItem>
+                                </UnorderedList>
+                                <Text mt='1rem' mb='1rem'>Les cat&eacute;gories sont toujours plus sp&eacute;cifiques que "MOTS DE 4 LETTRES" ou "ADJECTIFS".</Text>
+                                <Text mb='1rem'>Chaque puzzle a une unique solution. Attention aux pi&egrave;ges... Chaque groupe correspond &agrave; une couleur : </Text>
+                                <UnorderedList>
+                                    <ListItem>&#128993; : Facile</ListItem>
+                                    <ListItem>&#128994; : Moyen</ListItem>
+                                    <ListItem>&#128309; : Difficile</ListItem>
+                                    <ListItem>&#128995; : Tr&egrave;s difficile</ListItem>
+                                </UnorderedList>
                             </ModalBody>
                         </ModalContent>
                     </Modal>
-            <Stack>
-            {game.complete.map((group: Group) => (
-                <Stack
-                spacing={1}
-                lineHeight={1}
-                rounded="lg"
-                align="center"
-                justify="center"
-                h="80px"
-                w="624px"
-                bg={difficultyColor(group.difficulty)}
-                >
-                <Text fontSize="xl" fontWeight="extrabold" textTransform="uppercase">
-                    {group.category}
-                </Text>
-                <Text fontSize="xl" textTransform="uppercase">
-                    {group.items.join(', ')}
-                </Text>
+                    <Stack maxWidth="624px">
+                        {game.complete.map((group: Group) => (
+                            <Stack key={group.category} w={['368px', '448px', '528px', '624px']} h="80px" spacing={1} lineHeight={1} rounded="lg" align="center" justify="center" bg={difficultyColor(group.difficulty)}>
+                                <Text fontSize={["l", "xl"]} fontWeight="extrabold" textTransform="uppercase">{group.category}</Text>
+                                <Text fontSize={["l", "xl"]} textTransform="uppercase">{group.items.join(', ')}</Text>
+                            </Stack>
+                        ))}
+                        {chunk(game.items, 4).map((row, index) => (
+                            <HStack key={index} justify="center" spacing={4}>
+                                {row.map((item) => (
+                                    <Button key={item} w={['80px', '100px', '120px', '150px']} h="80px" bg="#efefe6" fontSize={["14px", "16px"]} fontWeight="extrabold" textTransform="uppercase" onClick={() => game.toggleActive(item)} isActive={game.activeItems.includes(item)} _active={{ bg: '#5a594e', color: 'white' }}>{item}</Button>
+                                ))}
+                            </HStack>
+                        ))}
+                    </Stack>
+                    <HStack align="baseline">
+                        <Text>Essais restants :</Text>
+                        {[...Array(game.mistakesRemaining).keys()].map((_, index) => (
+                            <Circle key={index} bg="gray.800" size="12px" />
+                        ))}
+                    </HStack>
+                    <HStack>
+                        <Button
+                            colorScheme="black"
+                            variant="outline"
+                            rounded="full"
+                            borderWidth="2px"
+                            onClick={game.shuffle}
+                        >
+                            M&eacute;langer
+                        </Button>
+                        <Button
+                            colorScheme="black"
+                            variant="outline"
+                            rounded="full"
+                            borderWidth="2px"
+                            isDisabled={game.activeItems.length <= 0}
+                            onClick={game.deselectAll}
+                        >
+                            D&eacute;selectionner tout
+                        </Button>
+                        <Button
+                            colorScheme="black"
+                            variant="outline"
+                            rounded="full"
+                            borderWidth="2px"
+                            isDisabled={game.activeItems.length !== 4}
+                            onClick={game.submit}
+                        >
+                            Valider
+                        </Button>
+                    </HStack>
                 </Stack>
-            ))}
-
-            {chunk(game.items, 4).map((row) => (
-                <>
-                <HStack>
-                    {row.map((item) => (
-                    <Button
-                        w="150px"
-                        h="80px"
-                        bg="#efefe6"
-                        fontSize="16px"
-                        fontWeight="extrabold"
-                        textTransform="uppercase"
-                        onClick={() => game.toggleActive(item)}
-                        isActive={game.activeItems.includes(item)}
-                        _active={{
-                        bg: '#5a594e',
-                        color: 'white',
-                        }}
-                    >
-                        {item}
-                    </Button>
-                    ))}
-                </HStack>
-                </>
-            ))}
-            </Stack>
-            <HStack align="baseline">
-            <Text>Essais restants :</Text>
-            {[...Array(game.mistakesRemaining).keys()].map(() => (
-                <Circle bg="gray.800" size="12px" />
-            ))}
-            </HStack>
-            <HStack>
-            <Button
-                colorScheme="black"
-                variant="outline"
-                rounded="full"
-                borderWidth="2px"
-                onClick={game.shuffle}
-            >
-                M&eacute;langer
-            </Button>
-            <Button
-                colorScheme="black"
-                variant="outline"
-                rounded="full"
-                borderWidth="2px"
-                isDisabled={game.activeItems.length <= 0}
-                onClick={game.deselectAll}
-            >
-                D&eacute;selectionner tout
-            </Button>
-            <Button
-                colorScheme="black"
-                variant="outline"
-                rounded="full"
-                borderWidth="2px"
-                isDisabled={game.activeItems.length !== 4}
-                onClick={game.submit}
-            >
-                Valider
-            </Button>
-            </HStack>
-        </Stack>
-        </Flex>
-    </ChakraProvider>
+            </Flex>
+        </ChakraProvider>
     );
 };
